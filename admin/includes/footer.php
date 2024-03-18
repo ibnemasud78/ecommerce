27b6b235bee2c9a331b8e69edc5d0c65cf76e772
ibnemasud78 <ibnemasud78@gmail.com>
2024-03-18 11:37:25 +0600

    initial commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2024 Brought to You By <a href="" class="text-danger">Masud</a></strong>
+</footer>
